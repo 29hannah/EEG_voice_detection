16ff66b851bf342f41eb449e93b6fb6a27c972d0
29hannah <hannah.ziesenies@gmail.com>
2024-02-08 10:49:12 +0100

    updated

--- experiment/config/vocoding_config.py
+++
@@ -1,8 +0,0 @@
-conditions = ['bandwidth-0.3',
-              'bandwidth-0.4',
-              'bandwidth-0.5',
-              'bandwidth-1.0',
-              'original']
-trial_number = 10
-trial_duration = 0.5
-deviant_freq = 0.1
--- experiment/config/voice_detetction_config.py
+++
@@ -1,8 +0,0 @@
-conditions = ['morph-0.0',
-              'morph-0.25',
-              'morph-0.5',
-              'morph-0.75',
-              'morph-1.0']
-trial_number = 10
-trial_duration = 2.0
-deviant_freq = 0.1
