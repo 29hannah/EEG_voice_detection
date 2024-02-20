ea6e474c40ac194b7da4ee82d785928a4ea19334
29hannah <hannah.ziesenies@gmail.com>
2024-02-20 14:24:01 +0100

    T1I1 seq slab + check

--- experiment/trial_sequence.py
+++ experiment/trial_sequence.py
@@ -1,4 +1,4 @@
-"""Generate a sequence in which each element has the same probability to follow one another (excluding deviant sound
+"""Generate a sequence in which each element has (almost always) the same probability to follow one another (excluding deviant sound
 Marker for deviant is n_conditions+1 = highest condition number
 """
 import numpy as np
@@ -8,7 +8,7 @@ import itertools
 
 
 # Generating the sequence
-def generate_sequence(n_conditions, n_reps, ):
+def generate_sequence(n_conditions, n_reps):
     elements = list(np.arange(1, n_conditions + 1))
     list_start = elements * n_reps
     list_start = list_start
@@ -67,52 +67,13 @@ def generate_opt_seq(n_conditions, n_reps):
     return sequence
 
 
-
-n_conditions= 11 # Including deviant!
-n_reps= 22 # has to be a multiple of n_conditions
-morph_seq = slab.Trialsequence(conditions=[1, 2, 3, 4, 5, 6], trials=generate_opt_seq(n_conditions, n_reps), n_reps=n_reps, deviant_freq=0)
-print(morph_seq.transitions())
-
-
-"""
-# Checking for correctness of sequence
-#Check for similarity between sequences
-sequences=[]
-ideal_trans = ((n_conditions * n_reps) / (n_conditions * n_conditions))
-def seq(n_conditions, n_reps):
-    eval = [1, 2]
-    while len(eval) > 1:
-        sequence, trans_dict = generate_sequence(n_conditions, n_reps)
-        trans_values = list(trans_dict.values())
-        eval = list()
-        for element in trans_values:
-            if element != ideal_trans:
-                eval.append(ideal_trans - element)
-    return sequence
-i=0
-for i in range(25):
-    sequence=seq(n_conditions, n_reps)
-    sequences.append(sequence)
-    i =i+1
-
-from difflib import SequenceMatcher
-def similar(a, b):
-    return SequenceMatcher(None, a, b).ratio()
-results=[]
-for i in range(len(sequences)-1):
-    seq_1=sequences[i]
-    seq_2=sequences[i+1]
-    res=similar(str(seq_1), str(seq_2))
-    results.append(res)
-
-
-# To check that all elements have the same frequency of occurence
-#from collections import Counter
-#res=Counter(sequence)
-# To visualize the trial sequence
-import matplotlib.pyplot as plt
-plt.figure()
-dates = list(trans_dict.keys())           # list() needed for python 3.x
-prices = list(trans_dict.values())        # ditto
-plt.plot(dates, prices) # this will show date at the x-axis
-"""
+def generate_slab_freq(n_conditions, n_reps):
+    conditions = list(np.arange(1, n_conditions + 1))
+    ideal_trans = ((n_conditions * n_reps) / (n_conditions * n_conditions))
+    morph_seq = slab.Trialsequence(conditions=conditions, trials=generate_opt_seq(n_conditions, n_reps),
+                                   n_reps=n_reps, deviant_freq=0)
+    while np.sum(morph_seq.transitions() != ideal_trans):
+        morph_seq = slab.Trialsequence(conditions=conditions,
+                                       trials=generate_opt_seq(n_conditions, n_reps),
+                                       n_reps=n_reps, deviant_freq=0)
+    return morph_seq
---
+++ experiment/trial_sequence_check.py
@@ -0,0 +1,61 @@
+""" Check for the correctness of the generated sequences
+- Equal occurrence of all elements
+- Check for similarity between sequences
+- Equal occurrence of transitions
+"""
+
+from experiment.trial_sequence import generate_slab_freq
+import matplotlib.pyplot as plt
+import numpy as np
+from collections import Counter
+from difflib import SequenceMatcher
+
+n_conditions= 6 # Including deviant!
+n_reps= 54# has to be a multiple of n_conditions
+
+# Generate 500 sequences
+sequences=[]
+for i in range(500):
+    print(i)
+    morph_seq=generate_slab_freq(n_conditions, n_reps)
+    sequences.append(morph_seq.trials)
+
+# To check that all elements have the same frequency of occurrence
+sum=[]
+for sequence in sequences:
+    res=[]
+    res=Counter(sequence)
+    i=1
+    results=[]
+    for i in range(len(res)):
+        i=i+1
+        results.append(res[i])
+    sum.append(results)
+for element in sum:
+    plt.plot(element)
+
+# Check how similar the sequences are
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
+results=[]
+for i in range(len(sequences)-1):
+    seq_1=sequences[i]
+    seq_2=sequences[i+1]
+    res=similar(str(seq_1), str(seq_2))
+    results.append(res)
+plt.plot(results)
+
+
+
+# Double check for correctness of transitions
+trans=[]
+ideal_trans = ((n_conditions * n_reps) / (n_conditions * n_conditions))
+for i in range(500):
+    print(i)
+    trans.append(np.sum( morph_seq.transitions() != ideal_trans))
+plt.plot(trans)
+
+
+
+
