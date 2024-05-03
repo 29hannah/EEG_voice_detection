e48420b30813646504bafc08b9cdb02d8821d143
Hannah Zis <101631916+29hannah@users.noreply.github.com>
2024-05-03 10:43:42 +0200

    Update voice_detetction_EEG_dis.py

--- experiment/voice_detetction_EEG_dis.py
+++ experiment/voice_detetction_EEG_dis.py
@@ -9,10 +9,10 @@ from EEG_voice_detection.experiment.config import get_config
 from EEG_voice_detection.experiment.trial_sequence import generate_slab_freq
 from datetime import datetime
 import pandas as pd
+import random
 
-
-participant_id = 'test_2202'
-stimulus_level=67
+participant_id = 'sub_02'
+stimulus_level=70
 
 
 slab.set_default_samplerate(44100)
@@ -86,12 +86,12 @@ results_folder_participant= results_folder + '\\' + participant_id
 if not os.path.isdir(results_folder_participant):
     os.makedirs(results_folder_participant)
 now=datetime.now()
-results_file_name = participant_id +  '_EEG_'+now.strftime('%y-%m-%d_%H-%M-%S')+'.csv'
+results_file_name = participant_id + '_EEG_'+now.strftime('%y-%m-%d_%H-%M-%S')+'.csv'
 
 
 #Get list of stimuli in slab format
 stim_dict=dict()
-stim_path= 'C:\\projects\\EEG_voice_detection\\experiment\\stimuli'
+stim_path= 'C:\\projects\\EEG_voice_detection\\experiment\\stimuli_peak_aligned'
 for sound_file in os.listdir(os.path.join(stim_path)):
     sound = slab.Sound.read(os.path.join(stim_path, sound_file))
     sound.level= stimulus_level
@@ -99,48 +99,53 @@ for sound_file in os.listdir(os.path.join(stim_path)):
     morph_ratio = sound_file[-7:-4]
     stim_dict[morph_ratio] = sound
 
+# Define the deviant sound
+deviant_sound = slab.Binaural('C:\\projects\\EEG_voice_detection\\experiment\\octave_0.5_morph-1.0_female.wav')
+deviant_sound.level = stimulus_level
+
 #morph_ratios= list(stim_dict.keys())
-morph_ratios= ['0.0',  '0.4', '0.5', '0.6', '1.0']
+morph_ratios= ['0.0', '0.4', '0.6', '1.0']
 #morph_seq = slab.Trialsequence(conditions=[1,2, 3, 4,5, 6], n_reps=5, deviant_freq=0.1)
 
-morph_seq=generate_slab_freq(n_conditions=6, n_reps=54)
+morph_seq=generate_slab_freq(n_conditions=4, n_reps=40)
 
 input('Press enter to start the experiment')
 
 results={}
 for morph in morph_seq:
     print('###Trial', morph_seq.this_n+1, '/', morph_seq.n_trials, '####')
-    if morph == 6: # Deviant is n_conditions+1
-        deviant_sound = slab.Binaural.chirp(duration=len(sound))
-        deviant_sound.level = stimulus_level
+
+    if random.randint(0,10)==10: # Play Deviant with probability of 10 %
         load_to_buffer(deviant_sound)
-        trig_value = len(morph_ratios) + 1
+        trig_value = len(morph_ratios) + 1 # Trigger Value Deviant is n_conditions+1
         freefield.write(tag='trigcode', value=trig_value, processors='RX82')
         freefield.play()
         print('Playing deviant')
         response, reaction_time = collect_response()
-        results[str(morph_seq.this_n)] = {'Response': response,
+        results[str(morph_seq.this_n)+'_deviant'] = {'Response': response,
                                           'Morph played':'Deviant',
                                           'Reaction time': reaction_time
                                           }
         #collect_responses(morph_seq, results_file)
         freefield.wait_to_finish_playing()
-    else:
-        stimulus = stim_dict[morph_ratios[morph-1]]
-        stimulus.level = stimulus_level
-        load_to_buffer(stimulus)
-        trig_value = morph
-        freefield.write(tag='trigcode', value=trig_value, processors='RX82')
-        freefield.play()
-        print('Playing morph ', morph_ratios[morph-1])
-        #collect_trial(morph_seq, results_file)
-        results[str(morph_seq.this_n)] = {'Response': 'Nan',
+
+    #print(results)
+
+    stimulus = stim_dict[morph_ratios[morph-1]]
+    stimulus.level = stimulus_level
+    load_to_buffer(stimulus)
+    trig_value = morph
+    freefield.write(tag='trigcode', value=trig_value, processors='RX82')
+    freefield.play()
+    print('Playing morph ', morph_ratios[morph-1])
+     #collect_trial(morph_seq, results_file)
+    results[str(morph_seq.this_n)] = {'Response': 'Nan',
                                           'Morph played': morph_ratios[morph - 1],
                                           'Reaction time':'Nan',
                                           'Participant': participant_id,
                                           'Phase': 'EEG'
                                           }
-        freefield.wait_to_finish_playing(proc="RP2", tag="playback")
+    freefield.wait_to_finish_playing(proc="RP2", tag="playback")
 
 
 results_df=pd.DataFrame.from_dict(results)
