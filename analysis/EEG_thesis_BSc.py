e443f8dac6daf73cd98475a32ddd742e06203f3d
29hannah <hannah.ziesenies@gmail.com>
2024-07-05 09:11:07 +0200

    BSc analysis

--- analysis/EEG_thesis_BSc.py
+++ analysis/EEG_thesis_BSc.py
@@ -1,31 +1,25 @@
 import json
-import matplotlib.pyplot as plt
-import mne
-from os import listdir
-import numpy as np
-from mne.stats import permutation_cluster_test, spatio_temporal_cluster_test
-from mne.channels import find_ch_adjacency
 import pandas as pd
 import pathlib
 import os
 import seaborn as sns
 import pythonpsignifitmaster.psignifit as ps
-from mne.channels import find_ch_adjacency, make_1020_channel_selections
-import scipy
+from mne.channels import make_1020_channel_selections
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats
 from mpl_toolkits.axes_grid1 import make_axes_locatable
-
 import mne
 from mne.channels import find_ch_adjacency
-from mne.datasets import sample
-from mne.stats import combine_adjacency, spatio_temporal_cluster_test
+from mne.stats import  spatio_temporal_cluster_test
 from mne.viz import plot_compare_evokeds
 
 
 # TODO check validity of used approach for clustering
 # TODO clusters and channel selection
+# TODO plot individual psychometric functions
+# Select channels manually: ch indx to name?
+
 
 # Function to fit psychometric function
 def fit_pf(data):
@@ -50,7 +44,7 @@ with open(DIR / "analysis" / "settings" / "preproc_config.json") as file:
     cfg = json.load(file)
 
 subjs=['sub_08', 'sub_09', 'sub_10', 'sub_11', 'sub_07', 'sub_12','sub_13', 'sub_14', 'sub_15', 'sub_16', 'sub_18',
-       'sub_19','sub_20', 'sub_21', 'sub_22', 'sub_23', 'sub_24']
+       'sub_19','sub_20', 'sub_21', 'sub_22', 'sub_23', 'sub_24', 'sub_25', 'sub_26', 'sub_27']
 
 # Plot the behavioural data/ Fit the psychometric function
 appended_data = []
@@ -130,8 +124,22 @@ mne.viz.plot_compare_evokeds(ignore_conds(
     evokeds_avrgd, "deviant"), combine='gfp')
 
 ## Permutation cluster test
-# All epochs, all subjects
 
+"""
+# Evokeds per subject/ one subject = one observation
+conditions = ['morph/0.0', 'morph/0.4','morph/0.6','morph/1.0']
+X_total= []
+for condition in conditions:
+    condition_idx=conditions.index(condition)
+    X_evoked=[]
+    for ele in enumerate(subjs):
+        subj_idx= ele[0]
+        evoked_sub= evokeds[condition][subj_idx].get_data().transpose()
+        X_evoked.append(evoked_sub)
+    X_total.append(np.stack( X_evoked, axis=0))
+"""
+
+# All epochs, all subjects
 # Get data of all epochs for one condition in one numpy array
 for subj in subjs:
     print(subj)
@@ -139,7 +147,7 @@ for subj in subjs:
     epochs = mne.read_epochs(epochs_folder / pathlib.Path(subj + '-epo.fif'))
     conditions = list(epochs.event_id.keys())[1:5]
     epochs.equalize_event_counts(conditions)
-    event_ids = [1, 2, 3, 4]
+    event_ids = [1,2,3,  4]
     indices = [np.where(epochs.events[:, 2] == event_id)[0] for event_id in event_ids]
     if subj=="sub_08": # = first subject
         X_total = [epochs.get_data()[idx, :, :].transpose(0, 2, 1) for idx in indices]
@@ -148,36 +156,94 @@ for subj in subjs:
         for event_id in event_ids:
             X_total[event_id-1]= np.concatenate((X_total[event_id-1], X[event_id-1]))
 
-# Spatiotemporal permutation cluster test/ mne example: descriptive and not statistically meaningful
 
+"""
+result= dict()
+for subj in subjs:
+    print(subj)
+    epochs_folder = EEG_DIR / subj / "epochs"
+    epochs = mne.read_epochs(epochs_folder / pathlib.Path(subj + '-epo.fif'))
+    conditions = list(epochs.event_id.keys())[1:5]
+    epochs.equalize_event_counts(conditions)
+    event_ids = [1, 4]
+    indices = [np.where(epochs.events[:, 2] == event_id)[0] for event_id in event_ids]
+    X_total = [epochs.get_data()[idx, :, :].transpose(0, 2, 1) for idx in indices]
+    # Spatiotemporal permutation cluster test/ mne example: descriptive and not statistically meaningful
+    adjacency, ch_names = find_ch_adjacency(epochs.info, ch_type="eeg")
+    #mne.viz.plot_ch_adjacency(epochs.info, adjacency, ch_names)
+    # Calculate statistical thresholds
+    F_obs, clusters, p_values, h0 = permutation_cluster_test(
+        X_total,
+        adjacency=adjacency,
+        n_permutations=100,
+        stat_fun=mne.stats.f_oneway
+        )
+
+    result[subj]= {
+            "f": F_obs,
+            "clusters": clusters,
+            "p": p_values,
+            "fs": epochs.info["sfreq"],
+            "t": epochs.times,
+        }
+
+
+results_over_subjects= []
+for subj in subjs:
+    p_values= result[subj]["p"]
+    F_obs= result[subj]["f"]
+    clusters= result[subj]["clusters"]
+
+    p_accept = 0.05
+    good_cluster_inds = np.where(p_values < p_accept)[0]
+    if len(good_cluster_inds)!=0:
+        for i_clu, clu_idx in enumerate(good_cluster_inds):
+            # unpack cluster information, get unique indices
+            time_inds, space_inds = np.squeeze(clusters[clu_idx])
+            ch_inds = np.unique(space_inds)
+            time_inds = np.unique(time_inds)
+
+            sig_times = epochs.times[time_inds]
+            tmin= min(sig_times)
+            tmax = max(sig_times)
+            results_over_subjects.append((subj, sig_times, tmin, tmax, ch_inds))
+
+df = pd.DataFrame(results_over_subjects, columns =['subj', 'sig_times', 'tmin', 'tmax', 'ch_inds'])
+"""
+
+# Based on https://mne.tools/dev/auto_tutorials/stats-sensor-space/75_cluster_ftest_spatiotemporal.html
 adjacency, ch_names = find_ch_adjacency(epochs.info, ch_type="eeg")
-#mne.viz.plot_ch_adjacency(epochs.info, adjacency, ch_names)
 
-# Calculate statistical thresholds
-F_obs, clusters, p_values, h0 = spatio_temporal_cluster_test(
+tail = 1
+
+alpha_cluster_forming = 0.05
+n_conditions = len(event_ids)
+n_observations = len(X_total[0])
+dfn = n_conditions - 1
+dfd = n_observations - n_conditions
+
+# Note: we calculate 1 - alpha_cluster_forming to get the critical value
+# on the right tail
+f_thresh = scipy.stats.f.ppf(1 - alpha_cluster_forming, dfn=dfn, dfd=dfd)
+
+cluster_stats = spatio_temporal_cluster_test(
     X_total,
-    threshold=None,
-    tail=1,
+    n_permutations=1000,
+    tail=tail,
+    n_jobs=None,
+    buffer_size=None,
     adjacency=adjacency,
-    n_permutations=100,
-    stat_fun=mne.stats.f_oneway
-    )
+)
 
-result= {
-        "f": F_obs,
-        "clusters": clusters,
-        "p": p_values,
-        "fs": epochs.info["sfreq"],
-        "t": epochs.times,
-    }
-np.save("/Users/hannahsmacbook/PycharmProjects/EEG_voice_detection/analysis/clustering/results_permutation-cluster-test.npy", result)
-data=  np.load("/Users/hannahsmacbook/PycharmProjects/EEG_voice_detection/analysis/clustering/results_permutation-cluster-test.npy", allow_pickle=True).item()
-F_obs,clusters, p_values, _, _ = data.values()
+F_obs, clusters, p_values, _ = cluster_stats
 
 # Subselect clusters
 p_accept = 0.05
 good_cluster_inds = np.where(p_values < p_accept)[0]
 print(len(good_cluster_inds))
+
+ch_inds_all= np.arange(0, 65, 1, dtype=int)
+
 
 # loop over clusters
 for i_clu, clu_idx in enumerate(good_cluster_inds):
@@ -191,7 +257,10 @@ for i_clu, clu_idx in enumerate(good_cluster_inds):
     sig_times = epochs.times[time_inds]
     # create spatial mask
     mask = np.zeros((f_map.shape[0], 1), dtype=bool)
-    mask[ch_inds, :] = True
+    #mask[ch_inds, :] = True
+    # manually
+    ch_inds_sub= np.array([13,52])
+    mask[ch_inds_sub, :] = True
 
     # initialize figure
     fig, ax_topo = plt.subplots(1, 1, figsize=(10, 3), layout="constrained")
@@ -200,8 +269,8 @@ for i_clu, clu_idx in enumerate(good_cluster_inds):
     f_evoked = mne.EvokedArray(f_map[:, np.newaxis], epochs.info, tmin=0)
     f_evoked.plot_topomap(
         times=0,
-        mask=mask,
         axes=ax_topo,
+        mask=mask,
         cmap="Reds",
         vlim=(np.min, np.max),
         show=False,
@@ -238,29 +307,38 @@ for i_clu, clu_idx in enumerate(good_cluster_inds):
     ymin, ymax = ax_signals.get_ylim()
     ax_signals.fill_betweenx(
         (ymin, ymax), sig_times[0], sig_times[-1], color="orange", alpha=0.3
+
     )
+
+    # Plot image plot
+    significant_points = np.zeros((351, 65), dtype=bool)
+    significant_points[time_inds, :] = True
+    significant_points[ ch_inds_all[ch_inds[np.searchsorted(ch_inds, ch_inds_all)] != ch_inds_all], :] = False
+    significant_points=np.swapaxes(significant_points, 0, 1)
+
+    selections = make_1020_channel_selections(combined_evokeds.info, midline="12z")
+    fig, axes = plt.subplots(nrows=3, figsize=(8, 8))
+    axes = {sel: ax for sel, ax in zip(selections, axes.ravel())}
+    time_unit = dict(time_unit="s")
+    combined_evokeds.plot_image(
+        axes=axes,
+        group_by=selections,
+        colorbar=False,
+        show=False,
+        mask=significant_points,
+        show_names="all",
+        titles=None,
+        **time_unit,
+    )
+    plt.colorbar(axes["Left"].images[-1], ax=list(axes.values()), shrink=0.3, label="µV")
+
 plt.show()
 
-
-time_unit = dict(time_unit="s")
-significant_points = p_values.reshape(F_obs.shape).T < 0.05
-selections = make_1020_channel_selections(combined_evokeds.info, midline="12z")
-fig, axes = plt.subplots(nrows=3, figsize=(8, 8))
-axes = {sel: ax for sel, ax in zip(selections, axes.ravel())}
-combined_evokeds.plot_image(
-    axes=axes,
-    group_by=selections,
-    colorbar=False,
-    show=False,
-    show_names="all",
-    titles=None,
-    **time_unit,
-)
 
 
 # Get amplitude measurements per subject
 tmin = 0.122
-tmax = 0.440
+tmax = 0.448
 channels=['Cz', 'CPz']
 results=[]
 for subj_idx in enumerate(subjs):
